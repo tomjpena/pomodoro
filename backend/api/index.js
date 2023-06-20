90597e51a55e22c210471ee6c21352c2c97ca4a7
tomjpena <thomasjamespenajr@gmail.com>
2023-06-19 23:13:08 -0500

    prep for vercel

---
+++ backend/api/index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const cors = require('cors')
+
+//Connect to DB
+connectDB()
+
+// Declare and initialize variables for server
+// @@ todo: setup PORT in env variables and use ||
+const PORT = 5000
+const app = express()
+
+app.use(cors(
+  {
+    origin: ["https://pomodoro-six-wine.vercel.app"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true
+  }
+))
+
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Cookie parser middleware
+app.use(cookieParser())
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use('/api/data', require('./routes/dataRoutes'))
+
+
+// Error handling middleware
+app.use(errorHandler)
+
+// Starting Server
+app.listen(0, () => {
+  console.log(`Server running on port: ${PORT}`);
+})
+
+// Handling Error
+process.on("unhandledRejection", err => {
+  console.log(`An error occurred: ${err.message}`)
+  server.close(() => process.exit(1))
+})
