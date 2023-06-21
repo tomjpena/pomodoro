01829976b9521ad48571a64faa6790d520a17bb3
tomjpena <thomasjamespenajr@gmail.com>
2023-06-21 00:25:54 -0500

    fixed cookie issue

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -19,15 +19,17 @@ const register = async (userData) => {
 const login = async (userData) => {
   // Post request for logging user in
   const response = await axios.post(`${API_URL}/login`, userData)
-
-  //If user data returns, put data into local storage
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
-    
-    const setCookieHeader = response.headers['set-cookie']
-    const jwtCookie = setCookieHeader.split(';')[0]
-      document.cookie = jwtCookie
-  }
+    .then(() => {
+      //If user data returns, put data into local storage
+      if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+        
+        const setCookieHeader = response.headers['set-cookie']
+        const jwtCookie = setCookieHeader.split(';')[0]
+          document.cookie = jwtCookie
+      }
+    }
+    )
   
 
   return response.data
