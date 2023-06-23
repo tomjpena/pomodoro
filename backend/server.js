b7a5cef37e88cd775db47504e073fc4a8cf66e13
tomjpena <thomasjamespenajr@gmail.com>
2023-06-22 23:12:19 -0500

    Changed cors origin for deployment of front end

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use("/", cors({
-  origin: "https://localhost:5173",
+  origin: "https://pomodoro-m6xi.onrender.com",
   credentials: true,
   exposedHeaders: ["Set-Cookie"]
 }));
