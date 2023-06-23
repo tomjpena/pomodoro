0f327af9127be7928401ce1aa0026db19bf50e13
tomjpena <thomasjamespenajr@gmail.com>
2023-06-22 22:37:30 -0500

    Change to vite configs

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use("/", cors({
-  origin: "http://localhost:5173",
+  origin: "https://localhost:5173",
   credentials: true,
   exposedHeaders: ["Set-Cookie"]
 }));
