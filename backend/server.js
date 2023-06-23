fccb26d558145128320dedba0cd8fb0006d0e97d
tomjpena <thomasjamespenajr@gmail.com>
2023-06-22 21:08:04 -0500

    Changed origin for testing

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use("/", cors({
-  origin: ['https://pomodoro-m6xi.onrender.com', "http://localhost:5173/"],
+  origin: "http://localhost:5173/",
   credentials: true,
   exposedHeaders: ["set-cookie"]
 }));
